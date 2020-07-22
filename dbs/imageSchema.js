4719d964616b2947afd7160d5b3c8caa627847c0
eliassong-laptop <eliassong1997@gmail.com>
2020-07-22 21:01:37 +0800

    image添加了parentId模式，添加了相册删除添加照片功能

--- dbs/imageSchema.js
+++ dbs/imageSchema.js
@@ -3,6 +3,7 @@ var mongoose = require('./db.js');
 Schema = mongoose.Schema;
 var imageSchema = new Schema({
   imageURL:{type:String},
+  parentId:{type:String},
   imageTime:{type:String,default:moment().format('YYYY-MM-DD hh:mm:ss')},
 });
 module.exports = mongoose.model('image',imageSchema,'images');
--- routes/album.js
+++ routes/album.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Album = require('../dbs/albumSchema')
+var Image = require("../dbs/imageSchema")
 /* GET users listing. */
 router.get('/search/album', function(req, res, next) {
   Album.find({},(err,album) => {
@@ -22,6 +23,81 @@ router.get('/search/album/detail/:id',(req,res,next) =>{
       res.send(album)
     }
   })
-})
+});
+
+router.post('/create/album',(req,res,next) => {
+  var album = new Album({
+    albumTitle:req.body.newAlbumTitle
+  })
+  album.save();
+  res.send("album created")
+});
+
+router.post('/create/album/image',(req,res,next) => {
+  const data = req.body;
+
+  var images=[]
+  // var temp = []
+  for(let i =0;i<data.newImageURLs.length;i++){
+    if(data.newImageURLs[i] !== ""){
+      var image = new Image({
+        imageURL:data.newImageURLs[i],
+        parentId:data.albumId
+      })
+      image.save()
+      images.push(image);
+    }
+  }
+
+  Album.findById(data.albumId,(err,album) => {
+    if(err){
+      console.log(err);
+    }
+    else{
+      for(let j=0;j<images.length;j++){
+        album.albumImage.push(images[j]);
+      }
+      album.save();
+    }
+  })
+  res.send("add image to album");
+});
+
+router.delete('/delete/album/:id',(req,res,next) => {
+  Image.deleteMany({parentId:req.params.id},err=>{
+    if(err){
+      console.log(err);
+    }
+  })
+  Album.findByIdAndDelete(req.params.id,(err) => {
+    if(err){
+      console.log(err);
+    }
+  })
+  res.send("删除成功")
+
+});
+
+router.delete('/delete/album/:albumId/image/:imageId',(req,res,next) => {
+ Image.findByIdAndDelete(req.params.imageId,err => {
+   if(err){
+     console.log(err);
+   }
+ })
+  Album.findById(req.params.albumId,(err,album) => {
+    if(err){
+      console.log(err);
+    }
+    else{
+      var idx = album.albumImage.findIndex(image => {
+        return image._id === req.params.imageId
+      })
+      album.albumImage.splice(idx,1);
+      album.save();
+      res.send("image deleted")
+    }
+  })
+
+});
 
 module.exports = router;
